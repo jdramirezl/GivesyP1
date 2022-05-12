40e54f5181346bf9e28d5a3af3465affd477787a
julian <jdramirezl@eafit.edu.co>
2022-05-12 01:14:33 -0500

    Functions for create and update

--- FRONTEND/src/utils/RestFactory.tsx
+++ FRONTEND/src/utils/RestFactory.tsx
@@ -3,22 +3,28 @@ import { baseURL } from "../config";
 import axios from "axios";
 import { useQuery } from "react-query";
 
+export async function Create<T>(name: string, body: object) {
+	return await axios.post<T>(`${baseURL}api/${name}/`, body);
+}
 
+export async function Update<T>(name: string, body: object, id: number) {
+	return await axios.put<T>(`${baseURL}api/${name}/${id}/`, body);
+}
 
-export function FetchAll<T> (name: string) {
-    const fetch = async () => {
-        const res = await axios.get<T[]>(`${baseURL}api/${name}/`);
-        return res.data;
-    };
-    const {data} = useQuery(`${name}`, fetch);
-    return data;
-};
+export function FetchAll<T>(name: string) {
+	const fetch = async () => {
+		const res = await axios.get<T[]>(`${baseURL}api/${name}/`);
+		return res.data;
+	};
+	const { data } = useQuery(`${name}`, fetch);
+	return data;
+}
 
-export function Fetch<T> (name: string) {
-    const fetch = async () => {
-        const res = await axios.get<T>(`${baseURL}api/${name}/`);
-        return res.data;
-    };
-    const {data} = useQuery(`${name}`, fetch);
-    return data;
-};
+export function Fetch<T>(name: string) {
+	const fetch = async () => {
+		const res = await axios.get<T>(`${baseURL}api/${name}/`);
+		return res.data;
+	};
+	const { data } = useQuery(`${name}`, fetch);
+	return data;
+}
